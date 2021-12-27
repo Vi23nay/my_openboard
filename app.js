736feba7a5d66c87a267c7819bae78e89dbd7b35
Vi23nay <vi23nay.vs@gmail.com>
2021-12-27 16:05:57 +0530

    added port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();//initialize and server ready
 
 app.use(express.static("public"));//frondend data-> main work
 
-let port = 5000;
+let port = process.env.PORT || 5000;//deployment
 let server = app.listen(port, () => {
     console.log('Listening to port ' + port);
 });
